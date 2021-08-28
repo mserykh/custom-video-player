2f18fa7e3e33ecb635a4fff73385bacfbb0ee3ce
Marie Serykh <marie.serykh@gmail.com>
2021-08-29 00:17:23 +0200

    feat: implement fullscreen with fading cursor

--- custom-video-player/index.js
+++ custom-video-player/index.js
@@ -1,15 +1,23 @@
 /* Elements */
 const player = document.querySelector('.video-player');
 const video = player.querySelector('.viewer');
+const controls = player.querySelector('.video-player__controls');
 const progress = player.querySelector('.video-player__progress');
 const progressBar = player.querySelector('.video-player__progress--filled');
 const toggles = player.querySelectorAll('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.video-player__slider');
+const fullscreenButton = player.querySelector('.video-player__btn--fullscreen');
+
+
+/* Flags */
+let mousTimer = null;
+let isHidden = false;
+let isMousedown = false;
 
 /* Functions */
 function togglePlay() {
-  if (video.paused) {
+  if (video.paused || video.ended) {
     video.play();
   }
   else {
@@ -48,6 +56,31 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+function toggleFullscreen() {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+    hide();
+  }
+  else if (document.webkitFullscrrenElement) {
+    document.webkitExitFullscreen();
+    hide();
+  }
+  else if (player.fullscreenElement) {
+    player.webkitExitFullscreen();
+    hide();
+  }
+  else {
+    player.requestFullscreen();
+    hide();
+  }
+}
+
+function hide() {
+  mouseTimer = null;
+  player.style.cursor = 'none';
+  isHidden = true;
+}
+
 /* Event listeners */
 // video.addEventListener('play', updateButton);
 // video.addEventListener('pause', updateButton);
@@ -57,7 +90,7 @@ toggles.forEach(toggle => toggle.addEventListener('click', togglePlay));
 
 skipButtons.forEach(skipButton => skipButton.addEventListener('click', skip));
 
-let isMousedown = false;
+
 ranges.forEach(range => range.addEventListener('input', handleRangeProgress));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
 ranges.forEach(range => range.addEventListener('click', handleRangeUpdate))
@@ -67,3 +100,16 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => isMousedown && scrub(e));
 progress.addEventListener('mousedown', () => isMousedown = true);
 progress.addEventListener('mouseup', () => isMousedown = false);
+
+fullscreenButton.addEventListener('click', toggleFullscreen);
+
+player.addEventListener('mousemove', function() {
+  if (mouseTimer) {
+    window.clearTimeout('mouseTimer');
+  }
+  if (isHidden) {
+    player.style.cursor = 'default';
+    isHidden = false;
+  }
+  mouseTimer = window.setTimeout(hide, 3000);
+});
