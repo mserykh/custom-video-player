78311fd08fe66ae1ff24d7a963d0a0933ac0014e
Maria Serykh <marie.serykh@gmail.com>
2022-07-20 21:30:49 +0200

    docs: update `README.md` and delete output to console

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# mserykh-JSFEPRESCHOOL
-Private repository for @mserykh
+# Custom Video Player
+HTML5 Video Player is created with HTML, CSS and JavaScript using HTMLMediaElement API.
+
+## Usage
+- key "Space" - toggle play/pause
+- key "k" - toggle play/pause
+- key "m" - toggle mute/unmute
+- key ">" - increase playback rate
+- key "<" - decrease playback rate
+- key "f" - toggle full screen
+- 0..9 - seek to specific point in the video
+- key "j" - rewind 10 seconds
+- key "l" - fast forward 25 seconds
+- key "r" - back to normal playback rate
+
+
+## Result
+Check the [player](https://mserykh-video-player.netlify.app/) out 
--- custom-video-player/index.js
+++ custom-video-player/index.js
@@ -1,20 +1,26 @@
 /* Elements */
-const player = document.querySelector('.video-player');
-const video = player.querySelector('.viewer');
-const controls = player.querySelector('.video-player__controls');
-const progress = player.querySelector('.video-player__progress');
-const progressBar = player.querySelector('.video-player__progress--filled');
-const toggle = player.querySelector('.toggle');
-const timeCodeCurrent = player.querySelector('.video-player__time-code--current');
-const timeCodeDuration = player.querySelector('.video-player__time-code--duration');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const volumeRange = player.querySelector('.video-player__slider--volume');
-const volumeButton = player.querySelector('.video-player__btn--volume');
-const volumePercentage = player.querySelector('.video-player__volume-percentage');
-const ranges = player.querySelectorAll('.video-player__slider');
-const speedRate = player.querySelector('.video-player__speed-rate');
-const speedRateButton = player.querySelector('.video-player__btn--speed');
-const fullscreenButton = player.querySelector('.video-player__btn--fullscreen');
+const player = document.querySelector(".video-player");
+const video = player.querySelector(".viewer");
+const controls = player.querySelector(".video-player__controls");
+const progress = player.querySelector(".video-player__progress");
+const progressBar = player.querySelector(".video-player__progress--filled");
+const toggle = player.querySelector(".toggle");
+const timeCodeCurrent = player.querySelector(
+  ".video-player__time-code--current"
+);
+const timeCodeDuration = player.querySelector(
+  ".video-player__time-code--duration"
+);
+const skipButtons = player.querySelectorAll("[data-skip]");
+const volumeRange = player.querySelector(".video-player__slider--volume");
+const volumeButton = player.querySelector(".video-player__btn--volume");
+const volumePercentage = player.querySelector(
+  ".video-player__volume-percentage"
+);
+const ranges = player.querySelectorAll(".video-player__slider");
+const speedRate = player.querySelector(".video-player__speed-rate");
+const speedRateButton = player.querySelector(".video-player__btn--speed");
+const fullscreenButton = player.querySelector(".video-player__btn--fullscreen");
 
 /* Flags and variables*/
 let isPlaying = false;
@@ -28,8 +34,7 @@ function togglePlay() {
     video.play();
     updateButton();
     isPlaying = true;
-  }
-  else {
+  } else {
     video.pause();
     updateButton();
     isPlaying = false;
@@ -39,11 +44,9 @@ function togglePlay() {
 function updateButton() {
   if (!isPlaying) {
     toggle.style.background = `url('assets/svg/play-small.svg')`;
-  }
-  else if (isPlaying) {
+  } else if (isPlaying) {
     toggle.style.background = `url('assets/svg/paused-small.svg')`;
   }
-  
 }
 
 function handleProgress() {
@@ -57,13 +60,12 @@ function handleProgress() {
     video.pause();
     updateButton();
     isPlaying = false;
-  };
-
+  }
 }
 
 function skip() {
-  if ((this.dataset.skip === '25') && video.currentTime >= (video.duration - 25)) {
-    video.currentTime += (video.duration - video.currentTime - 0.02);
+  if (this.dataset.skip === "25" && video.currentTime >= video.duration - 25) {
+    video.currentTime += video.duration - video.currentTime - 0.02;
     handleProgress();
   } else {
     video.currentTime += parseFloat(this.dataset.skip);
@@ -73,7 +75,7 @@ function skip() {
 function handleRangeUpdate() {
   const value = this.value;
   video[this.name] = value;
-  if (this.name === 'volume') lastVolumeValue = value;
+  if (this.name === "volume") lastVolumeValue = value;
 }
 
 function handleRangeProgress() {
@@ -83,8 +85,8 @@ function handleRangeProgress() {
   const percent = Math.floor(((value - min) / (max - min)) * 100);
   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${percent}%, #C4C4C4 ${percent}%)`;
 
-  if (this.name === 'volume') showVolumePercentage(percent);
-  if (this.name === 'playbackRate') showSpeedRate(value);
+  if (this.name === "volume") showVolumePercentage(percent);
+  if (this.name === "playbackRate") showSpeedRate(value);
 }
 
 function showVolumePercentage(percent) {
@@ -98,39 +100,40 @@ function showSpeedRate(value) {
 
 function toggleVolume() {
   if (isVolumeOn) {
-    volumeButton.classList.add('muted');
+    volumeButton.classList.add("muted");
     video.muted = isVolumeOn;
     ranges[0].value = 0;
     ranges[0].style.background = `linear-gradient(to right, #710707 0%, #710707 ${ranges[0].value}%, #C4C4C4 ${ranges[0].value}%)`;
     showVolumePercentage(Math.floor(ranges[0].value * 100));
     isVolumeOn = false;
-  }
-  else if (!isVolumeOn) {
-    volumeButton.classList.remove('muted');
+  } else if (!isVolumeOn) {
+    volumeButton.classList.remove("muted");
     video.muted = isVolumeOn;
     video.volume = lastVolumeValue;
     ranges[0].value = lastVolumeValue;
-    ranges[0].style.background = `linear-gradient(to right, #710707 0%, #710707 ${ranges[0].value * 100}%, #C4C4C4 ${ranges[0].value * 100}%)`;
+    ranges[0].style.background = `linear-gradient(to right, #710707 0%, #710707 ${
+      ranges[0].value * 100
+    }%, #C4C4C4 ${ranges[0].value * 100}%)`;
     showVolumePercentage(Math.floor(ranges[0].value * 100));
     isVolumeOn = true;
   }
 }
 
 function toggleVolumeIcon(percent) {
-  if (percent < 51 & percent !== 0) {
-    volumeButton.classList.remove('muted');
-    volumeButton.classList.remove('volume-high');
-    volumeButton.classList.add('volume-low');
+  if ((percent < 51) & (percent !== 0)) {
+    volumeButton.classList.remove("muted");
+    volumeButton.classList.remove("volume-high");
+    volumeButton.classList.add("volume-low");
     isVolumeOn = true;
     video.muted = !isVolumeOn;
   } else if (percent >= 51) {
-    volumeButton.classList.remove('muted');
-    volumeButton.classList.remove('volume-low');
-    volumeButton.classList.add('volume-high');
+    volumeButton.classList.remove("muted");
+    volumeButton.classList.remove("volume-low");
+    volumeButton.classList.add("volume-high");
     isVolumeOn = true;
     video.muted = !isVolumeOn;
   } else if (percent === 0) {
-    volumeButton.classList.add('muted');
+    volumeButton.classList.add("muted");
     isVolumeOn = false;
     video.muted = !isVolumeOn;
   }
@@ -140,14 +143,18 @@ function backToNormalSpeedRate() {
   video.playbackRate = 1;
   ranges[1].value = +video.playbackRate;
   showSpeedRate(+video.playbackRate);
-  ranges[1].style.background = `linear-gradient(to right, #710707 0%, #710707 ${(video.playbackRate - 0.5) / 1.5 * 100}%, #C4C4C4 ${(video.playbackRate - 0.5) / 1.5 * 100}%)`;
+  ranges[1].style.background = `linear-gradient(to right, #710707 0%, #710707 ${
+    ((video.playbackRate - 0.5) / 1.5) * 100
+  }%, #C4C4C4 ${((video.playbackRate - 0.5) / 1.5) * 100}%)`;
 }
 
 function displayReadableTime(currentTime) {
   const hours = Math.floor(currentTime / 3600);
   const minutes = Math.floor(currentTime / 60);
   const seconds = Math.floor(currentTime % 60);
-  timeCodeCurrent.innerHTML = `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}`;
+  timeCodeCurrent.innerHTML = `${formatNumber(hours)}:${formatNumber(
+    minutes
+  )}:${formatNumber(seconds)}`;
 }
 
 function formatNumber(number) {
@@ -159,7 +166,9 @@ function getDuration() {
   const hours = Math.floor(duration / 3600);
   const minutes = Math.floor(duration / 60);
   const seconds = Math.floor(duration % 60);
-  timeCodeDuration.innerHTML = `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}`;
+  timeCodeDuration.innerHTML = `${formatNumber(hours)}:${formatNumber(
+    minutes
+  )}:${formatNumber(seconds)}`;
 }
 
 function scrub(e) {
@@ -171,16 +180,13 @@ function toggleFullscreen() {
   if (document.fullscreenElement) {
     document.exitFullscreen();
     fullscreenButton.style.background = `url('assets/svg/flscreen.svg')`;
-  }
-  else if (document.webkitFullscrrenElement) {
+  } else if (document.webkitFullscrrenElement) {
     document.webkitExitFullscreen();
     fullscreenButton.style.background = `url('assets/svg/flscreen.svg')`;
-  }
-  else if (player.fullscreenElement) {
+  } else if (player.fullscreenElement) {
     player.webkitExitFullscreen();
     fullscreenButton.style.background = `url('assets/svg/flscreen.svg')`;
-  }
-  else {
+  } else {
     player.requestFullscreen();
     fullscreenButton.style.background = `url('assets/svg/exit-flscreen.svg')`;
   }
@@ -189,149 +195,119 @@ function toggleFullscreen() {
 function onKeyElementClick(e) {
   e.preventDefault();
 
-  if (e.code === 'Space' || e.code === 'KeyK') {
+  if (e.code === "Space" || e.code === "KeyK") {
     togglePlay();
   }
 
-  if (e.code === 'KeyJ') {
+  if (e.code === "KeyJ") {
     video.currentTime -= 10;
     if (video.paused) {
       togglePlay();
     }
   }
 
-  if (e.code === 'KeyL') {
+  if (e.code === "KeyL") {
     skip.call(skipButtons[1]);
   }
 
-  if (e.code === 'KeyM') {
+  if (e.code === "KeyM") {
     toggleVolume();
   }
 
-  if (e.key === '>') {
+  if (e.key === ">") {
     if (video.playbackRate >= 2) return;
     video.playbackRate += parseFloat(ranges[1].step);
     ranges[1].value = +video.playbackRate;
     showSpeedRate(+video.playbackRate);
-    ranges[1].style.background = `linear-gradient(to right, #710707 0%, #710707 ${(video.playbackRate - 0.5) / 1.5 * 100}%, #C4C4C4 ${(video.playbackRate - 0.5) / 1.5 * 100}%)`;
+    ranges[1].style.background = `linear-gradient(to right, #710707 0%, #710707 ${
+      ((video.playbackRate - 0.5) / 1.5) * 100
+    }%, #C4C4C4 ${((video.playbackRate - 0.5) / 1.5) * 100}%)`;
   }
 
-  if (e.key === '<') {
+  if (e.key === "<") {
     if (video.playbackRate <= 0.5) return;
     video.playbackRate -= parseFloat(ranges[1].step);
     ranges[1].value = +video.playbackRate;
     showSpeedRate(+video.playbackRate);
-    ranges[1].style.background = `linear-gradient(to right, #710707 0%, #710707 ${(video.playbackRate - 0.5) / 1.5 * 100}%, #C4C4C4 ${(video.playbackRate - 0.5) / 1.5 * 100}%)`;
+    ranges[1].style.background = `linear-gradient(to right, #710707 0%, #710707 ${
+      ((video.playbackRate - 0.5) / 1.5) * 100
+    }%, #C4C4C4 ${((video.playbackRate - 0.5) / 1.5) * 100}%)`;
   }
 
-  if (e.code === 'KeyR') {
+  if (e.code === "KeyR") {
     backToNormalSpeedRate();
   }
 
-  if (e.code === 'KeyF') {
+  if (e.code === "KeyF") {
     toggleFullscreen();
   }
 
-  if (e.code === 'Digit0' || e.code === 'Numpad0') {
+  if (e.code === "Digit0" || e.code === "Numpad0") {
     video.currentTime = 0;
   }
 
-  if (e.code === 'Digit1' || e.code === 'Numpad1') {
+  if (e.code === "Digit1" || e.code === "Numpad1") {
     video.currentTime = video.duration * 0.1;
   }
 
-  if (e.code === 'Digit2' || e.code === 'Numpad2') {
+  if (e.code === "Digit2" || e.code === "Numpad2") {
     video.currentTime = video.duration * 0.2;
   }
 
-  if (e.code === 'Digit3' || e.code === 'Numpad3') {
+  if (e.code === "Digit3" || e.code === "Numpad3") {
     video.currentTime = video.duration * 0.3;
   }
 
-  if (e.code === 'Digit4' || e.code === 'Numpad4') {
+  if (e.code === "Digit4" || e.code === "Numpad4") {
     video.currentTime = video.duration * 0.4;
   }
 
-  if (e.code === 'Digit5' || e.code === 'Numpad5') {
+  if (e.code === "Digit5" || e.code === "Numpad5") {
     video.currentTime = video.duration * 0.5;
   }
 
-  if (e.code === 'Digit6' || e.code === 'Numpad6') {
+  if (e.code === "Digit6" || e.code === "Numpad6") {
     video.currentTime = video.duration * 0.6;
   }
 
-  if (e.code === 'Digit7' || e.code === 'Numpad7') {
+  if (e.code === "Digit7" || e.code === "Numpad7") {
     video.currentTime = video.duration * 0.7;
   }
 
-  if (e.code === 'Digit8' || e.code === 'Numpad8') {
+  if (e.code === "Digit8" || e.code === "Numpad8") {
     video.currentTime = video.duration * 0.8;
   }
 
-  if (e.code === 'Digit9' || e.code === 'Numpad9') {
+  if (e.code === "Digit9" || e.code === "Numpad9") {
     video.currentTime = video.duration * 0.9;
   }
-
 }
 
 /* Event listeners */
-document.addEventListener('keydown', onKeyElementClick);
+document.addEventListener("keydown", onKeyElementClick);
 
-video.addEventListener('timeupdate', handleProgress);
-video.addEventListener('click', togglePlay);
-video.addEventListener('loadeddata', getDuration)
+video.addEventListener("timeupdate", handleProgress);
+video.addEventListener("click", togglePlay);
+video.addEventListener("loadeddata", getDuration);
 
-toggle.addEventListener('click', togglePlay);
+toggle.addEventListener("click", togglePlay);
 
-volumeButton.addEventListener('click', toggleVolume);
+volumeButton.addEventListener("click", toggleVolume);
 
-skipButtons.forEach(skipButton => skipButton.addEventListener('click', skip));
+skipButtons.forEach((skipButton) => skipButton.addEventListener("click", skip));
 
-speedRateButton.addEventListener('click', backToNormalSpeedRate);
+speedRateButton.addEventListener("click", backToNormalSpeedRate);
 
-ranges.forEach(range => range.addEventListener('input', handleRangeProgress));
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('click', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove', () => isMousedown && handleRangeUpdate));
+ranges.forEach((range) => range.addEventListener("input", handleRangeProgress));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) => range.addEventListener("click", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", () => isMousedown && handleRangeUpdate)
+);
 
-progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e) => isMousedown && scrub(e));
-progress.addEventListener('mousedown', () => isMousedown = true);
-progress.addEventListener('mouseup', () => isMousedown = false);
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => isMousedown && scrub(e));
+progress.addEventListener("mousedown", () => (isMousedown = true));
+progress.addEventListener("mouseup", () => (isMousedown = false));
 
-fullscreenButton.addEventListener('click', toggleFullscreen);
-
-console.log(`
-Rolling Scopes School - Stage 0 task
-
-Self-evaluation by Maria Serykh
-
-✅ reproduce functionality of the original application - 10 points 
-✅ add mandatory additional functionality - 10 points:
-    [+] key "Space" - toggle play/pause
-    [+] key "k" - toggle play/pause
-    [+] key "m" - toggle mute/unmute
-    [+] key ">" - increase playback rate
-    [+] key "<" - decrease playback rate
-    [+] key "f" - toggle full screen
-✅implement additional functionality - 10 points: 
-    [+] 0..9 - seek to specific point in the video - 2 points
-    [+] key "j" - rewind 10 seconds - 2 points
-    [+] key "l" - fast forward 25 seconds - 2 points
-    [+] key "r" - back to normal playback rate - 2 points
-
-    [+] show time elapsed and video duration - 2 points
-
-    Total: 30 points
-
-****To do****
-    [ ] implement slider
-    [ ] implement control sound volume with keyboard
-    [ ] add transitions and hover effects
-    [ ] show time code while scrubbing
-    [ ] show description of actions triggerd by the keyboard (mute / mute, playback rate, etc.)
-    [ ] make the player adaptive
-    [ ] refactor code
-    
-developed by mserykh - 2021
-`);
+fullscreenButton.addEventListener("click", toggleFullscreen);
